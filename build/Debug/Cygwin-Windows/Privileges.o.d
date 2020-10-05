c0003513de45bf4ff62f33ff812da02b1e60c37b
Lucas Fitzgerald <LucasFitzgerald@my.unt.edu>
2020-10-05 13:54:46 -0500

    Intital Commit4

---
+++ build/Debug/Cygwin-Windows/Privileges.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin-Windows/Privileges.o: Privileges.cpp Privileges.h
+
+Privileges.h:
---
+++ build/Debug/Cygwin-Windows/display_layout.o.d
@@ -0,0 +1,4 @@
+build/Debug/Cygwin-Windows/display_layout.o: display_layout.cpp \
+ display_layout.h
+
+display_layout.h:
